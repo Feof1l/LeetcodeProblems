592abc24c85ead015d30f7bcf0277afd215649ab
Feof1l <ivan.vasilov.03@mail.ru>
2026-01-21 16:35:06 +0300

    Contains Duplicate

---
+++ 217.Contains Duplicate/main.go
@@ -0,0 +1,11 @@
+package main
+
+// 217 https://leetcode.com/problems/contains-duplicate/description/
+// Given an integer array nums, return true if any value appears
+// at least twice in the array, and return false if every element is distinct.
+// Input: nums = [1,2,3,1]
+
+// Output: true
+func main() {
+
+}
