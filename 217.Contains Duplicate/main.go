448d39bffbb5b53acb19866bc39fb4168d008c2d
Feof1l <ivan.vasilov.03@mail.ru>
2026-01-21 16:54:06 +0300

    Contains Duplicate

--- 217.Contains Duplicate/main.go
+++ 217.Contains Duplicate/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "log"
+
 // 217 https://leetcode.com/problems/contains-duplicate/description/
 // Given an integer array nums, return true if any value appears
 // at least twice in the array, and return false if every element is distinct.
@@ -7,5 +9,19 @@ package main
 
 // Output: true
 func main() {
+	log.Println(hasDuplicate([]int{}))
+}
 
+func hasDuplicate(nums []int) bool {
+	dict := make(map[int]bool, len(nums))
+
+	for _, num := range nums {
+		if _, ok := dict[num]; ok {
+			return true
+		}
+
+		dict[num] = true
+	}
+
+	return false
 }
