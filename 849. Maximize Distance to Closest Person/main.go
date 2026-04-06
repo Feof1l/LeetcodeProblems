5cc292d9c5efb1388e6990a63aa7214f4c3d0758
Feof1l <ivan.vasilov.03@mail.ru>
2026-04-06 21:55:24 +0300

    849. Maximize Distance to Closest Person

---
+++ 849. Maximize Distance to Closest Person/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "log"
+
+func main() {
+	log.Println(maxDistToClosest([]int{0, 1, 1, 0, 0, 0}))
+}
+
+func maxDistToClosest(seats []int) int {
+	currLen := 0
+	maxLen := 0
+	for i := 0; i < len(seats); i++ {
+		if seats[i] == 1 {
+			currLen = 0
+		} else {
+			currLen++
+			if (currLen+1)/2 > maxLen {
+				maxLen = (currLen + 1) / 2
+			}
+		}
+	}
+
+	flag := true
+	for i := 0; i < len(seats) && flag; i++ {
+		if seats[i] == 1 {
+			if i > maxLen {
+				maxLen = i
+			}
+			flag = false
+		}
+	}
+
+	flag = true
+	for i := len(seats) - 1; i >= 0 && flag; i-- {
+		if seats[i] == 1 {
+			if len(seats)-1-i > maxLen {
+				maxLen = len(seats) - 1 - i
+			}
+			flag = false
+		}
+	}
+
+	return maxLen
+}
